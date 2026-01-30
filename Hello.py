0173ee25ca6d5adaca0309e58f8647d00fb375fc
PratheekKamatam <245123737181@mvsrec.edu.in>
2026-01-30 00:56:47 -0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,1 @@
-print("Ninnu sadivichindi enduku?",end=" ")
-print("assembly lo andaru english lo matladtunnaru, nuvvu kuda english matladtavani", end=" ")
-print("assembly ki vellaraa ante ammerica ki potanu antav enti ra", end=" ")
-print("KOJJA!")
+print("hello world")
